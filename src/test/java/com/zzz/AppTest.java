2e4c6177b5254320ef40c5dfcbc65afc7e532c6a
bj <bj@126.com>
2021-12-21 23:29:09 +0800

    初始化

---
+++ src/test/java/com/zzz/AppTest.java
@@ -0,0 +1,20 @@
+package com.zzz;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test01(){
+        System.out.println("初始化......");
+    }
+
+}
